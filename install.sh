27fe53c20712ce75bf65689728e91a661c9e8586
Pedro Parra Ortega <pedro.parraortega@enreach.com>
2025-01-29 08:12:02 +0100

    fix script

--- install.sh
+++ install.sh
@@ -14,9 +14,10 @@ sudo rm -rf $HOME/homebrew/plugins/Decky-Framegen
 echo "installing Decky-Framegen plugin"
 
 curl -L $(curl -s https://api.github.com/repos/xXJSONDeruloXx/Decky-Framegen/releases/latest | grep "browser_download_url" | cut -d '"' -f 4) -o $HOME/Decky-Framegen.tar.gz
-sudo tar -xzf Decky-Framegen.tar.gz -C $HOME/homebrew/plugins
+sudo unzip Decky-Framegen.zip
+sudo mv Decky-Framegen/Decky-Framegen $HOME/homebrew/plugins/
 
-rm  $HOME/Decky-Framegen.tar.gz
+rm -rf $HOME/Decky-Framegen.zip __MACOSX Decky-Framegen
 sudo systemctl restart plugin_loader.service
 
 echo "Installation complete"
